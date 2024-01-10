bdd4eb8327eb5ddb4a675577e22848d66851382a
adamlieske <adam.lieske@yahoo.com>
2024-01-11 00:21:37 +0100

    config jako niewidzialny

--- .config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  db: "mongodb+srv://admin:OGj52lPotkRAGwrf@cluster0.dafm1bj.mongodb.net/?retryWrites=true&w=majority",
-  keySession: ["11121997"],
-  maxAgeSession: 24 * 60 * 60 * 1000,
-};
--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
-const config = require("./.config");
+const config = require("./config");
 const mongoose = require("mongoose");
 const colors = require("colors");
 
