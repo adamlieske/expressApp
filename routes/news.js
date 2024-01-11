df8acda854395c5cf7cde1ee5b45d4e783754667
adamlieske <adam.lieske@yahoo.com>
2024-01-11 23:38:28 +0100

    news. filter and front

--- routes/news.js
+++ routes/news.js
@@ -1,9 +1,21 @@
 const express = require("express");
 const router = express.Router();
+const News = require("../models/news.js");
 
-/* GET home page. */
-router.get("/", (req, res) => {
-  res.render("news", { title: "News" });
+router.get("/", async (req, res) => {
+  try {
+    const search = req.query.search || "";
+
+    const data = await News.find({
+      title: new RegExp(search.trim(), "i"),
+    }).sort({
+      created: -1,
+    });
+
+    res.render("news", { title: "Admin", data, search });
+  } catch (err) {
+    console.error("Error".red, err);
+  }
 });
 
 module.exports = router;
