5f0921a423bb70b88b5a5dba77ec31458cef4500
adamlieske <adam.lieske@yahoo.com>
2024-01-12 23:40:07 +0100

    api test 3

--- routes/api.js
+++ routes/api.js
@@ -4,15 +4,13 @@ const News = require("../models/news.js");
 
 router.get("/", async (req, res) => {
   try {
-    const search = req.query.search || "";
-
     const data = await News.find({
       title: new RegExp(search.trim(), "i"),
     }).sort({
       created: -1,
     });
 
-    res.json({ data });
+    res.json(data);
   } catch (err) {
     console.error("Error".red, err);
   }
